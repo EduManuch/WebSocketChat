3565f4d58dc39f41920d45deafb1353f317b655b
Eduard <eduard@email.com>
2026-02-12 01:14:43 +0300

    Delete createTopic from producer.go. Add kafka-init to docker compose and creating topic

--- internal/wsserver/producer.go
+++ internal/wsserver/producer.go
@@ -1,11 +1,9 @@
 package wsserver
 
 import (
-	"context"
 	"encoding/json"
 	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 	log "github.com/sirupsen/logrus"
-	"time"
 )
 
 func NewProducer(address string) (*kafka.Producer, error) {
@@ -21,10 +19,6 @@ func NewProducer(address string) (*kafka.Producer, error) {
 	p, err := kafka.NewProducer(conf)
 	if err != nil {
 		panic(err)
-	}
-	err = createTopic(conf)
-	if err != nil {
-		return nil, err
 	}
 	return p, nil
 }
@@ -54,27 +48,6 @@ func (ws *wsSrv) sendToKafka(message *WsMessage) {
 	if err != nil {
 		kafkaDropped.Inc()
 	}
-}
-
-func createTopic(conf *kafka.ConfigMap) error {
-	admin, err := kafka.NewAdminClient(conf)
-	if err != nil {
-		return err
-	}
-	defer admin.Close()
-
-	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
-	defer cancel()
-
-	_, err = admin.CreateTopics(ctx, []kafka.TopicSpecification{
-		{
-			Topic:             "web-topic",
-			NumPartitions:     1,
-			ReplicationFactor: 1,
-		},
-	})
-
-	return err
 }
 
 func (ws *wsSrv) GetProducerEventsKafka() {
